b2a96c14741ab230da42161ea05abdfb2f16b5ca
AIE\s221308 <cameron18556@gmail.com>
2023-05-04 14:46:35 +0930

    got rid of unused code

--- RaylibStarterCPP/main.cpp
+++ RaylibStarterCPP/main.cpp
@@ -91,22 +91,43 @@ int main(int argc, char* argv[])
         }
 
 
-        if (!twoPlayer && started) {
-            //check input for all rackets
-            if (ball.y + ball.height / 2 > player2.y + player2.height / 2) {
-                player2.y += racketSpeed * deltaTime;
-            }
-            
-            if (ball.y + ball.height / 2 < player2.y + player2.height / 2) {
-                player2.y -= racketSpeed * deltaTime;
-            }            
+        if ( started) {
 
-            if (IsKeyDown(KEY_S)) {
-                player1.y += racketSpeed * deltaTime;
-            }
+            if (!twoPlayer)
+            {
+                //check input for all rackets
+                if (ball.y + ball.height / 2 > player2.y + player2.height / 2) {
+                    player2.y += racketSpeed * deltaTime;
+                }
 
-            if (IsKeyDown(KEY_W)) {
-                player1.y -= racketSpeed * deltaTime;
+                if (ball.y + ball.height / 2 < player2.y + player2.height / 2) {
+                    player2.y -= racketSpeed * deltaTime;
+                }
+
+                if (IsKeyDown(KEY_S)) {
+                    player1.y += racketSpeed * deltaTime;
+                }
+
+                if (IsKeyDown(KEY_W)) {
+                    player1.y -= racketSpeed * deltaTime;
+                }
+            }
+            else {
+                if (IsKeyDown(KEY_DOWN)) {
+                    player2.y += racketSpeed * deltaTime;
+                }
+
+                if (IsKeyDown(KEY_UP)) {
+                    player2.y -= racketSpeed * deltaTime;
+                }
+
+                if (IsKeyDown(KEY_S)) {
+                    player1.y += racketSpeed * deltaTime;
+                }
+
+                if (IsKeyDown(KEY_W)) {
+                    player1.y -= racketSpeed * deltaTime;
+                }
             }
 
             //move rackets on screen if they went off
@@ -270,186 +291,7 @@ int main(int argc, char* argv[])
                 }
             }
         }
-        else if (started)
-        {
-        //check input for all rackets
-        if (IsKeyDown(KEY_DOWN)) {
-            player2.y += racketSpeed * deltaTime;
-        }
-
-        if (IsKeyDown(KEY_UP)) {
-            player2.y -= racketSpeed * deltaTime;
-        }
-
-        if (IsKeyDown(KEY_S)) {
-            player1.y += racketSpeed * deltaTime;
-        }
-
-        if (IsKeyDown(KEY_W)) {
-            player1.y -= racketSpeed * deltaTime;
-        }
-
-        //move rackets on screen if they went off
-        if (player1.y < 0)
-        {
-            player1.y = 0;
-        }
-
-        if (player1.y > screenHeight - 120) {
-            player1.y = screenHeight - 120;
-        }
-
-        if (player2.y < 0)
-        {
-            player2.y = 0;
-        }
-
-        if (player2.y > screenHeight - 120) {
-            player2.y = screenHeight - 120;
-        }
-
-        //move ball
-        if (p1win == false && p2win == false)
-        {
-            ball.y -= ballSpeed.y * deltaTime;
-            ball.x += ballSpeed.x * deltaTime;
-        }
-
-        //check ball colisions and change direction and speed accordingly
-        if (ball.y <= 0)
-        {
-            ballSpeed.y *= -1;
-
-            //whenever you see this bit it means the ball is speeding up
-            if (ballSpeed.y < 0)
-            {
-                ballSpeed.y -= speedPerBump;
-            }
-            else {
-                ballSpeed.y += speedPerBump;
-            }
-
-            if (ballSpeed.x < 0)
-            {
-                ballSpeed.x -= speedPerBump;
-            }
-            else {
-                ballSpeed.x += speedPerBump;
-            }
-        }
-
-        if (ball.y >= screenHeight - 20)
-        {
-            ballSpeed.y *= -1;
-            if (ballSpeed.y < 0)
-            {
-                ballSpeed.y -= speedPerBump;
-            }
-            else {
-                ballSpeed.y += speedPerBump;
-            }
-
-            if (ballSpeed.x < 0)
-            {
-                ballSpeed.x -= speedPerBump;
-            }
-            else {
-                ballSpeed.x += speedPerBump;
-            }
-        }
-
-        if (CheckCollisionRecs(player1, ball)) {
-            ///this section changes the direction of the ball so that it will go in a certain direction depending on where it hits the paddle
-            float ballPaddleDiffY = (player1.y + player1.height / 2) - (ball.y + ball.height / 2);
-            float ballPaddleDiffX = (ball.x + ball.width / 2) - (player1.x + player1.width / 2);
-
-            float origanalMagnitude = sqrt(ballSpeed.x * ballSpeed.x + ballSpeed.y * ballSpeed.y);
-
-            //we set ballSpeed to the distances here becuase when normailized they become the direction
-            ballSpeed.y = ballPaddleDiffY;
-            ballSpeed.x = ballPaddleDiffX;
-
-            float triangleMagnitude = sqrt(ballSpeed.x * ballSpeed.x + ballSpeed.y * ballSpeed.y);
-
-            //mormalizing ballSpeed
-            ballSpeed.y /= triangleMagnitude;
-            ballSpeed.x /= triangleMagnitude;
-
-            //we put the origanal speed back on ballSpeed with the new direction
-            ballSpeed.y *= origanalMagnitude;
-            ballSpeed.x *= origanalMagnitude;
-
-            if (ballSpeed.y < 0)
-            {
-                ballSpeed.y -= speedPerBump;
-            }
-            else {
-                ballSpeed.y += speedPerBump;
-            }
-
-            if (ballSpeed.x < 0)
-            {
-                ballSpeed.x -= speedPerBump;
-            }
-            else {
-                ballSpeed.x += speedPerBump;
-            }
-        }
-
-        if (CheckCollisionRecs(player2, ball)) {
-
-            float ballPaddleDiffY = (player2.y + player2.height / 2) - (ball.y + ball.height / 2);
-            float ballPaddleDiffX = (ball.x + ball.width / 2) - (player2.x + player2.width / 2);
-
-            float origanalMagnitude = sqrt(ballSpeed.x * ballSpeed.x + ballSpeed.y * ballSpeed.y);
-
-            ballSpeed.y = ballPaddleDiffY;
-            ballSpeed.x = ballPaddleDiffX;
-
-            float triangleMagnitude = sqrt(ballSpeed.x * ballSpeed.x + ballSpeed.y * ballSpeed.y);
-
-            ballSpeed.y /= triangleMagnitude;
-            ballSpeed.x /= triangleMagnitude;
-
-            ballSpeed.y *= origanalMagnitude;
-            ballSpeed.x *= origanalMagnitude;
-
-
-            if (ballSpeed.y < 0)
-            {
-                ballSpeed.y -= speedPerBump;
-            }
-            else {
-                ballSpeed.y += speedPerBump;
-            }
-
-            if (ballSpeed.x < 0)
-            {
-                ballSpeed.x -= speedPerBump;
-            }
-            else {
-                ballSpeed.x += speedPerBump;
-            }
-        }
-
-        if (ball.x <= 0) {
-            ++player2Score.score;
-            ball.x = screenWidth / 2 - 10;
-            ball.y = screenHeight / 2 - 10;
-            if (player2Score.score == 4) {
-                p2win = true;
-            }
-        }
-
-        if (ball.x >= screenWidth - 10) {
-            ++player1Score.score;
-            ball.x = screenWidth / 2 - 10;
-            ball.y = screenHeight / 2 - 10;
-            if (player1Score.score == 4) {
-                p1win = true;
-            }
-        }
-        }
+        
         //----------------------------------------------------------------------------------
 
         // Draw
